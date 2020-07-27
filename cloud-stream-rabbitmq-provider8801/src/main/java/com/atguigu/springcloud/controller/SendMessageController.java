5f68b1989a14fb8d14e3675daba8211b14baaf16
wangyi <wangyi@ghcchina.com.cn>
2020-07-27 14:14:40 +0800

    springcloud_learning_phase2: add SpringCloud Stream rabbitMQ 8801 provider

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/controller/SendMessageController.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.service.IMessageProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SendMessageController {
+
+    @Autowired
+    private IMessageProvider messageProvider;
+
+    @GetMapping("/sendMessage")
+    public String sendMessage(){
+        return messageProvider.send();
+    }
+}
