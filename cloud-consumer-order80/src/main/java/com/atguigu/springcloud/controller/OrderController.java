f24b2a6ba84b6c7d3baa37d2f53b3b113deb432b
wangyi <wangyi@ghcchina.com.cn>
2020-07-28 17:38:54 +0800

    springcloud_learning_phase2: SpringCloud zipkin+sleuth add code and config in 8001 and 80

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -78,10 +78,10 @@ public class OrderController
     }
 //
 //    // ====================> zipkin+sleuth
-//    @GetMapping("/consumer/payment/zipkin")
-//    public String paymentZipkin()
-//    {
-//        String result = restTemplate.getForObject("http://localhost:8001"+"/payment/zipkin/", String.class);
-//        return result;
-//    }
+    @GetMapping("/consumer/payment/zipkin")
+    public String paymentZipkin()
+    {
+        String result = restTemplate.getForObject("http://localhost:8001"+"/payment/zipkin/", String.class);
+        return result;
+    }
 }
--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -86,9 +86,9 @@ public class PaymentController
 //        return serverPort;
 //    }
 //
-//    @GetMapping("/payment/zipkin")
-//    public String paymentZipkin()
-//    {
-//        return "hi ,i'am paymentzipkin server fall back，welcome to atguigu，O(∩_∩)O哈哈~";
-//    }
+    @GetMapping("/payment/zipkin")
+    public String paymentZipkin()
+    {
+        return "hi ,i'am paymentzipkin server fall back，welcome to atguigu，O(∩_∩)O哈哈~";
+    }
 }
